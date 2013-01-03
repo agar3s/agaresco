cc44641ecbb36eb379d4d72d81b61e0e3bf70ff8
Agares <ekeisco@gmail.com>
2013-01-03 02:11:18 -0500

    simple markdown reader for my blog

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('De navidad quiero un mindstorm\n');
-}).listen(8000, '127.0.0.1');
+var express = require('express');
+var md = require('node-markdown').Markdown;
+var fs = require("fs");
+var app = express();
+
+app.get('/', function(req, res){
+
+  fs.readFile('post/primer_post.md', 'utf8', function (err,data) {
+  	var body = "";
+	if (err) {
+	  console.log(err);
+	  body = err.toString();
+	}else{
+	  body = md(data);
+	}
+	res.setHeader('Content-Type', 'text/html; charset=utf-8');
+	res.setHeader('Content-Length', body.length);
+	res.end(body);
+  });
+
+});
+
+app.get('/:id', function(req, res) {
+  // Then you can use the value of the id with req.params.id
+  // So you use it to get the data from your database:
+  var body = 'Hello: '+req.params.id;
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
+
+app.listen(8000);
 console.log('Server running at http://127.0.0.1:8000/');
---
+++ post/primer_post.md
@@ -0,0 +1,13 @@
+# Hello blog
+
+## probando los diferentes headers
+
+esto está muy cool por que puedo escribir en markdown y luego los renderizo en
+html con un estilo predeterminado.
+
+**Tareas pendientes**
+
+* hacer que el contenido este embebido
+* colocar un estilo simple
+* crear una forma para indexar los diferentes posts, navegarlos y verlos de 
+manera individual
